eb3bd0e7938686a09e92e2e55de18df74bd6ad49
tp-pro <tpasquier.pro@gmail.com>
2024-07-14 14:25:12 +0200

    get movie title, release date, vote and image failed

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@ import MediaCard from "@/components/MediaCard/MediaCard";
 import PostList from "@/components/PostList";
 import styles from "./page.module.css";
 import ClientComponent from "./ClientComponent";
+import Popular from "@/components/Popular/Popular";
 
 export default function Home() {
     // throw new Error("error")
     return (
         <div className={styles.main}>
+            <Popular />
             <p>{process.env.NEXT_PUBLIC_APPLICATION_HOST}</p>
-            <MediaCard mediaId={10} />
+            {/* <MediaCard mediaId={10} /> */}
             <p>composants serveurs / clients</p>
             <ClientComponent />
             <PostList />
